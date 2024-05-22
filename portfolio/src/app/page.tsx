1d8d0c70f5698e9399cdf5f1dd454057b5d40b89
TK-Vinh <vinhtkse171805@fpt.edu.vn>
2024-05-22 14:26:40 +0700

    ADD: changes

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,14 +1,15 @@
 
-import { GlobalStateProvider } from './component/contexts/GlobalStateContext';
-import About from './component/layout/About';
-import Footer from './component/layout/Footer';
-import Hero from './component/layout/Hero';
-import Modal from './component/layout/Modal';
-import Navbar from './component/layout/Navbar';
-import Projects from './component/layout/Projects';
-import Services from './component/layout/Services';
-import BurgerMenu from './component/menu/BurgerMenu';
+
+import { GlobalStateProvider } from '@/component/contexts/GlobalStateContext';
 import './globals.css'
+import Navbar from '@/component/layout/Navbar';
+import Modal from '@/component/layout/Modal';
+import BurgerMenu from '@/component/menu/BurgerMenu';
+import Hero from '@/component/layout/Hero';
+import About from '@/component/layout/About';
+import Projects from '@/component/layout/Projects';
+import Services from '@/component/layout/Services';
+import Footer from '@/component/layout/Footer';
 
 
 export default function Home() {
