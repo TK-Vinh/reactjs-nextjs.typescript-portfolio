efdaaea7f4cfcf61a3a6b5bb1c47397fbd1ae09a
TK-Vinh <98683007+TK-Vinh@users.noreply.github.com>
2025-10-20 04:19:11 +0700

    Adjust featured projects layout to three-column row

--- portfolio/src/component/layout/Projects.tsx
+++ portfolio/src/component/layout/Projects.tsx
@@ -23,7 +23,7 @@ function Projects() {
                 </p>
             </div>
 
-            <ul className="mt-14 grid list-none gap-10 sm:grid-cols-2 xl:grid-cols-3" role="list">
+            <ul className="mt-14 grid list-none gap-10 md:grid-cols-3" role="list">
                 {projectData.data.map((item, index) => {
                     const isHovered = hoveredIndex === index;
 
